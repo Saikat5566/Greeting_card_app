fd1205dc97af63a002f7a7ac273e246cea539cf3
Muhammad Abir Hasan Saikat <me.abirhasansaikat0024@gmail.com>
2025-11-25 17:29:37 +0600

    first commit

--- lib/app/modules/edit_profile/views/edit_profile_view.dart
+++ lib/app/modules/edit_profile/views/edit_profile_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:greeting_card_app/app/modules/common_widget/custom_eleveted_button.dart';
+import 'package:greeting_card_app/app/modules/common_widget/custom_text_field.dart';
 
 import '../controllers/edit_profile_controller.dart';
 
@@ -20,10 +22,75 @@ class EditProfileView extends GetView<EditProfileController> {
           ),
         ),
       ),
-      body: Column(children: [
-        SizedBox(height: 24,),
-        Container()
-      ],)
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Stack(
+                alignment: Alignment.bottomRight,
+                children: [
+                  Container(
+                    height: 160,
+                    width: 160,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80),
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/profile image.png'),
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xFFEBE0FF),
+                    ),
+                    child: Icon(
+                      Icons.camera_alt_rounded,
+                      color: Color(0xFF5A00FF),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(height: 24),
+
+            CustomTextField(
+              text: 'Full Name',
+              hintText: 'Sarah Anderson',
+              icon: Icon(Icons.person_2_outlined),
+            ),
+
+            CustomTextField(
+              text: 'Email Address',
+              hintText: 'sarah@email.com',
+              icon: Icon(Icons.email_outlined),
+            ),
+
+            CustomTextField(
+              text: 'Phone Number',
+              hintText: '+12569874745',
+              icon: Icon(Icons.call),
+            ),
+
+            SizedBox(height: 145),
+
+            CustomElevetedButton(
+              text: 'Save Changes',
+              color: Color(0xFF5A00FF),
+              buttonSize: Size(double.infinity, 52),
+              textColor: Colors.white,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
